dd626615190ef7a24ae5a7dd59f678265aab75cd
Fajar Muharam <fajarmuharam14@gmail.com>
2022-09-22 12:57:05 +0700

    portfolio fajar tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors : {
+        primary: '#14B8A6',
+        dark: '#0F172A',
+        secondary: '#64748b'
+      }
+    },
+  },
+  plugins: [],
+}
